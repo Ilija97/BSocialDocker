1c886abfc7b507bccadfe9197e03f2ae36415cfa
Ilija Gracanin <ilija.gracanin@cikom.com>
2024-02-28 08:15:38 +0100

    elastic search index check

--- ElasticSearchMicroservice/index.js
+++ ElasticSearchMicroservice/index.js
@@ -20,14 +20,35 @@ const username = process.env.ELASTICSEARCH_USERNAME || 'ilija';
 const password = process.env.ELASTICSEARCH_PASSWORD || 'ilijaG';
 const esClient = new Client({ node: elasticsearchHost, auth: { username: username, password: password } });
 
+const indexExists = async (indexName) => {
+  try {
+    return await esClient.indices.exists({ index: indexName });
+  } catch (error) {
+    console.error('Error checking if index exists:', error);
+    throw error; 
+  }
+};
+
 const createIndex = async (indexName) => {
-  await esClient.indices.create({ index: indexName });
-  console.log("Index created");
+  const indexAlreadyExists = await indexExists(indexName);
+
+  if (!indexAlreadyExists) {
+    try {
+      await esClient.indices.create({ index: indexName });
+      console.log(`Index "${indexName}" created`);
+    } catch (error) {
+      console.error(`Error creating index "${indexName}":`, error);
+      throw error; // You might want to handle this error appropriately
+    }
+  } else {
+    console.log(`Index "${indexName}" already exists.`);
+  }
 };
 
 createIndex('user');
 createIndex('post');
 createIndex('comment');
+
 
 // Middleware for parsing JSON
 app.use(bodyParser.json());
